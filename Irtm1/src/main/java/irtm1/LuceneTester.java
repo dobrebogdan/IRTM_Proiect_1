b3d7419ed49d1f6a06b830f5fe16766345cb8e69
Dobre Bogdan <dobrebogdan98@gmail.com>
2021-11-14 12:25:37 +0200

    Added some useful code

--- Irtm1/src/main/java/irtm1/LuceneTester.java
+++ Irtm1/src/main/java/irtm1/LuceneTester.java
@@ -1,18 +1,46 @@
 package irtm1;
-
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.analysis.ro.RomanianAnalyzer;
+
+import org.tartarus.snowball.ext.RomanianStemmer;
 
 public class LuceneTester
 {
     String indexDir = "./Index"; String dataDir = "./Data";
     Indexer indexer;
     Searcher searcher;
-    public static void main(String[] args)
+    public static List<String> analyze(String text, Analyzer analyzer) throws IOException{
+        List<String> result = new ArrayList<String>();
+        TokenStream tokenStream = analyzer.tokenStream("", text);
+        CharTermAttribute attr = tokenStream.addAttribute(CharTermAttribute.class);
+        tokenStream.reset();
+        while(tokenStream.incrementToken()) {
+            result.add(attr.toString());
+        }
+        return result;
+    }
+    public static void main(String[] args) throws IOException
     {
+        RomanianAnalyzer romanianAnalyzer = new RomanianAnalyzer();
+        List<String> results = analyze("Ceva text foarte interesant aici sau altceva legat de porci", romanianAnalyzer);
+        System.out.println(results);
+
+        /*RomanianStemmer romanianStemmer = new RomanianStemmer();
+        String x = "acestora";
+        romanianStemmer.setCurrent(x);
+        System.out.println(romanianStemmer.stem());
+        System.out.println(romanianStemmer.getCurrent());
+        */
         LuceneTester tester;
         try
         {
